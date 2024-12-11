e7f6ca4bd8869038da680b96a38232ac8d4ac38b
TheFatAndFurious <mrguerrilla@gmail.com>
2024-12-11 23:03:00 +0100

    website model

--- ovalie/news/admin.py
+++ ovalie/news/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Article
+from .models import Website
 
 
 @admin.register(Article)
@@ -7,3 +8,8 @@ class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'link', 'published_at', 'website', 'is_visible', 'is_featured', 'image')
     list_filter = ('is_visible', 'is_featured', 'website', 'image')
     search_fields = ('title', 'website')
+
+@admin.register(Website)
+class WebsiteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url', 'logo')
+
--- ovalie/news/models.py
+++ ovalie/news/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+class Website(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    logo = models.ImageField(upload_to='logos/')
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
     link = models.URLField(unique=True)  # Unique constraint
     published_at = models.DateTimeField(auto_now_add=True)
-    website = models.CharField(max_length=255)
+    website = models.ForeignKey(Website, on_delete=models.CASCADE)
     is_visible = models.BooleanField(default=True)
     admin_comment = models.TextField(null=True)
     is_featured = models.BooleanField(default=False)
