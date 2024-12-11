27497607f455ec4f22bf2d58fa11d06cd1f9abc5
TheFatAndFurious <mrguerrilla@gmail.com>
2024-12-11 13:30:33 +0100

    pagination

--- ovalie/news/views.py
+++ ovalie/news/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from .models import Article
-
+from django.core.paginator import Paginator
 
 def article_list(request):
+    # fetch articles from the DB
     articles = Article.objects.order_by('-published_at')
+
+    # fetch featured articles
     featured_articles = Article.objects.filter(is_featured=True).order_by('-published_at')[:3]
+
+    #pagination
+    paginator = Paginator(articles, 25)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+
     return render(request, 'news/articles_list.html', {
-        'articles': articles,
+        'page_obj': page_obj,
         'featured_articles': featured_articles})
 
 
