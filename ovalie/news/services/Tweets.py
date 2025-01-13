0ae28a706cfb0e1ea9f3f24e4f0b3946620e04cd
TheFatAndFurious <mrguerrilla@gmail.com>
2025-01-13 23:28:57 +0100

    tweets

--- ovalie/news/services/Tweets.py
+++ ovalie/news/services/Tweets.py
@@ -1,23 +1,25 @@
-import tweepy
+import tweepy, time
 
 bearer_token="AAAAAAAAAAAAAAAAAAAAAM1FyAEAAAAALAj1E2DzriaoMF7MfNMppzVPGQw%3DCH36Odh3QjSRe5bkx1h1RzRHqAVcsoITXuP6gAXpoEaPKyDZaN"
 
-client = tweepy.Client(bearer_token)
+def get_user_id(name):
+    client = tweepy.Client(bearer_token)
+    user = client.get_user(username=name)
+    return user.data.id
 
-# Get User's Tweets
+def get_tweets_from_user(user_id):
+    client = tweepy.Client(bearer_token)
+    response = client.get_users_tweets(user_id)
+    if response:
+        for tweet in response.data:
+            print(tweet.id)
+            print(tweet.text)
 
-# This endpoint/method returns Tweets composed by a single user, specified by
-# the requested user ID
 
-user_id = 2244994945
+username = "StadeToulousain"
+user_id = get_user_id(username)
+if user_id:
+    print(f"user id is {user_id}")
+    time.sleep(2)
+get_tweets_from_user(user_id)
 
-response = client.get_users_tweets(user_id)
-
-# By default, only the ID and text fields of each Tweet will be returned
-for tweet in response.data:
-    print(tweet.id)
-    print(tweet.text)
-
-# By default, the 10 most recent Tweets will be returned
-# You can retrieve up to 100 Tweets by specifying max_results
-response = client.get_users_tweets(user_id, max_results=10)
