1eacdc250a9df083483e73b22ab81a828a461db5
TheFatAndFurious <mrguerrilla@gmail.com>
2025-01-18 23:46:30 +0100

    vite

---
+++ ovalie/paparugby-vite/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    '../news/templates/**/*.html', // Django templates
+    '../static/js/**/*.js',   // Custom JS files
+    './src/**/*.{js,ts,jsx,tsx}', // Vite source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+};
---
+++ ovalie/paparugby-vite/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: '../static/news', // Output directory for your Django static files
+    emptyOutDir: true,        // Clear the output directory before building
+  },
+  server: {
+    watch: {
+      usePolling: true,       // Necessary for some environments (e.g., Docker)
+    },
+    strictPort: true,          // Ensures the dev server uses the specified port
+    port: 5173,               // Vite's default port
+  },
+});
