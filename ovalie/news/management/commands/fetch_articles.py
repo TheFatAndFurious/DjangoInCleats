a3d96d647236a9ed6cf42ed06decd60afb43f3df
TheFatAndFurious <mrguerrilla@gmail.com>
2025-01-24 21:38:57 +0100

    getting angrey forever and ever

--- ovalie/news/management/commands/fetch_articles.py
+++ ovalie/news/management/commands/fetch_articles.py
@@ -3,10 +3,9 @@ import sys, os, django
 
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ovalie.settings')
 django.setup()
 
-from news.scrapers.news_scraper import (run_all_scrapers)
+from ovalie.news.scrapers.news_scraper import (run_all_scrapers)
 
 class Command(BaseCommand):
     help = "Fetch articles"
--- ovalie/news/management/commands/fetch_rugby_data.py
+++ ovalie/news/management/commands/fetch_rugby_data.py
@@ -1,9 +1,8 @@
 from django.core.management.base import BaseCommand
 import sys, os, django
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ovalie.settings')
 django.setup()
-from news.services.tasks import RugbyDevsTasks
+from ovalie.news.services.tasks import RugbyDevsTasks
 class Command(BaseCommand):
     help = "Fetch rugby data"
 
