bafa19fa93832c3d4abb36f6aa34b2237b317a5e
TheFatAndFurious <mrguerrilla@gmail.com>
2025-01-24 21:41:04 +0100

    getting angrey forever and ever

--- ovalie/news/apps.py
+++ ovalie/news/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class NewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'news'  # Ensure this matches the app folder name
+    name = 'ovalie.news'  # Ensure this matches the app folder name
