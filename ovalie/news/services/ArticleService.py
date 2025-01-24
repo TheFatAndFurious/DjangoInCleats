a6badbd200d95f097f0a9b9a23e35c8bac33eaaa
TheFatAndFurious <mrguerrilla@gmail.com>
2025-01-24 22:13:23 +0100

    light at the end of the tunnel

--- ovalie/news/services/ArticleService.py
+++ ovalie/news/services/ArticleService.py
@@ -6,7 +6,7 @@ from django.db.models import Q
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ovalie.settings')
 django.setup()
-from news.models import Article
+from ovalie.news.models import Article
 
 class ArticleService:
     @staticmethod
