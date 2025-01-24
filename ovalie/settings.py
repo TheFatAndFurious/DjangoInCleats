08adca951c3ccac3aafb35f20129f447f293920d
TheFatAndFurious <mrguerrilla@gmail.com>
2025-01-24 22:57:26 +0100

    light at the end of the tunnel

--- ovalie/settings.py
+++ ovalie/settings.py
@@ -55,11 +55,13 @@ INSTALLED_APPS = [
     'ovalie.news.apps.NewsConfig',
     'django_browser_reload',
     'django_q',
-    'django.contrib.sitemaps'
+    'django.contrib.sitemaps',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,6 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",  # Global static directory (if used)
     BASE_DIR / "news/static/news",  # Add your app's static directory
