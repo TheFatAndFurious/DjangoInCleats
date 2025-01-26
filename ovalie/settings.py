da4ba7167ff9ad3312c2dc43720fe1b463df28be
TheFatAndFurious <mrguerrilla@gmail.com>
2025-01-26 11:00:22 +0100

    nginx to serve static

--- ovalie/settings.py
+++ ovalie/settings.py
@@ -54,12 +54,10 @@ INSTALLED_APPS = [
     'django_browser_reload',
     'django_q',
     'django.contrib.sitemaps',
-    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
