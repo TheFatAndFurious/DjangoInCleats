6e794935d55695ebf0f281ef10d6db82054b0386
TheFatAndFurious <mrguerrilla@gmail.com>
2025-02-07 15:24:02 +0100

    theme switcher: fix

--- ovalie/news/static/news/js/script.js
+++ ovalie/news/static/news/js/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggleBtn = document.getElementById('theme-toggle');
+    const togglTxt = document.getElementById('togglTxt')
     let storedTheme = localStorage.getItem('theme') || 'cyberpunk';
     document.documentElement.setAttribute('data-theme', storedTheme);
 
@@ -8,5 +9,10 @@ document.addEventListener("DOMContentLoaded", () => {
       storedTheme = (storedTheme === 'cupcake') ? 'business' : 'cupcake';
       document.documentElement.setAttribute('data-theme', storedTheme);
       localStorage.setItem('theme', storedTheme);
+      if (storedTheme === 'cupcake') {
+        togglTxt.innerHTML = 'Dark'
+      } else {
+        togglTxt.innerHTML = 'Light'
+      }
     });
   });
